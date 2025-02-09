cce4d28e8ff9649c1fbec3d260b5b9f79e46a41e
khusheebhartee06 <bhartikhushi32@gmailcom>
2025-02-09 17:36:50 +0530

    add student review

---
+++ assest/js/script.js
@@ -0,0 +1,34 @@
+new Swiper('.slider-wrapper', {
+    loop: true,
+    gapCursor: true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 200000,
+        disableOnInteraction: false
+    },
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBultets: true
+    },
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+          },
+    }
+});
