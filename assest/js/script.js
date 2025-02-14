0d28316519f6390b2b6ec60f85b5ac303c74bdba
khusheebhartee06 <bhartikhushi32@gmailcom>
2025-02-14 12:17:46 +0530

    add multiple section 14 feb 2025

--- assest/js/script.js
+++ assest/js/script.js
@@ -1,31 +1,68 @@
+//Student Review start 
 new Swiper('.slider-wrapper', {
-    loop: true,
-    gapCursor: true,
-    spaceBetween: 20,
-    autoplay: {
-        delay: 2000,
-        disableOnInteraction: false
+  loop: true,
+  gapCursor: true,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false
+  },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBultets: true
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1
     },
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        dynamicBultets: true
+    640: {
+      slidesPerView: 1
     },
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+    768: {
+      slidesPerView: 2
     },
-    breakpoints: {
-        640: {
-            slidesPerView: 1
-          },
-          768: {
-            slidesPerView: 2
-          },
-          1024: {
-            slidesPerView: 3
-          },
-    }
+    1024: {
+      slidesPerView: 3
+    },
+  }
 });
+// Student Review start end
+// Why choose section start
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  gapCursor: true,
+  spaceBetween: 0,
+  autoplay: {
+    delay: 1000,
+    disableOnInteraction: false
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  }, navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1
+    },
+    640: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2
+    },
+    1024: {
+      slidesPerView: 3
+    },
+  }
+});
+// Why choose section end
